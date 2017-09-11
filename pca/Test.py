07e3be545ba644b7a39d425bc4ca844ceef252c0
natalia navas <natinavas@gmail.com>
2017-09-11 00:58:55 -0300

    Algorithm implemented using numpy functions
    Not working, the image import is not working correctly

---
+++ pca/Test.py
@@ -0,0 +1,49 @@
+import numpy
+from PIL import Image
+
+THRESHOLD = 0.9
+
+# Load images
+images = list(None for i in range(1,410))
+for i in range(1,41):
+    for j in range(1,11):
+        dir= "/Users/natinavas/Documents/ITBA/MNA/orl_faces/s"+str(i)+"/"+str(j)+".pgm"
+        print(list(Image.open(dir).getdata()))
+        images[(i-1)*10+(j-1)]=list(Image.open(dir).getdata())
+
+# Create matrix out of images
+matrix = numpy.transpose(numpy.matrix(images))
+
+print('2----')
+# print(matrix)
+
+#Calculate mean for rows
+mean = matrix.mean(axis=1)
+centered_matrix = matrix - mean
+print('3----')
+
+#Calculate the covariance matrix
+#TODO: hacer bien
+covariance_matrix = numpy.cov(centered_matrix)
+print('4----')
+
+# Calculate eigen values
+#TODO: hacer bien
+#OBS: se esta asumiendo que los eigen values estan ordenados.
+eig_values, eig_vectors = numpy.linalg.eig(covariance_matrix)
+print('5----')
+
+# Get best eigenvalues
+sum_eig_values = sum(eig_values)
+actual_sum = 0
+i = 0
+print('6----')
+while(actual_sum/sum_eig_values < THRESHOLD):
+    actual_sum += eig_values[i]
+    i+=1
+best_eig_vectors = eig_vectors[:, 0:i]
+print('7----')
+# Project values on eigen vectors
+projected_values = numpy.transpose(centered_matrix)*best_eig_vectors
+
+print(projected_values)
