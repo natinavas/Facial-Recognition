3fca499c146663e38b20960cad9604105f7e6b6a
natalia navas <natinavas@gmail.com>
2017-09-16 20:09:52 -0300

    Fix

--- pca/PCA.py
+++ pca/PCA.py
@@ -9,9 +9,8 @@ from qr import GrahamSchmidt as gs
 """http://www.face-rec.org/algorithms/pca/jcn.pdf"""
 
 # Define parser and get input values
-DEFAULT_PATH = "../att_faces/s"
 parser = argparse.ArgumentParser(description='Facial Recognition software with PCA')
-parser.add_argument('--images', '-i', type=str, default=DEFAULT_PATH, dest="images")
+parser.add_argument('--images', '-i', type=str, default="../att_faces/s", dest="images")
 parser.add_argument('--image_type', '-it', type=str, default='.pgm', dest="image_type")
 parser.add_argument('--training_set_size', '-tss', type=int, default=5, dest="training_set_size")
 parser.add_argument('--eig_method', '-em', type=str, dest="method", default="householder")
