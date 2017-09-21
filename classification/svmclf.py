8b47e412313c0e2fdaa3145f09dedf6644d1b21b
natalia navas <natinavas@gmail.com>
2017-09-21 16:59:45 -0300

    Add print with correct classifications

--- classification/svmclf.py
+++ classification/svmclf.py
@@ -21,25 +21,6 @@ def svmclassify(training_set, training_class, testing_set, testing_class, verbos
     clf.fit(training_set, training_class)
 
     classifications = clf.score(testing_set, testing_class)
-
-    print ("Correct classifications", classifications)
-    #testing_class #
-    #Setup for control
-    # check, error = 0 , 0
-    # error_matrix = np.zeros((classCount,classCount))
-    #
-    # #For each testing element
-    # for i in range (len(testing_set)):
-    #
-    #     #Get prediction
-    #     prediction = clf.predict(testing_set[i])
-    #
-    #     error_matrix[testing_class[i] - 1, prediction - 1]+=1
-    #     if (verbose==1):
-    #         print ("Predicting item {} , {} as {}".format(i, testing_class[i], prediction))
-    #     if (testing_class[i] == prediction):
-    #         check+=1
-    #     else:
-    #         error+=1
-    #
-    # return (check, error, error_matrix)
+    print "----------------------------------------"
+    print "Correct Classifications", classifications
+    print "----------------------------------------"
